f1e2de91a4a1fea0a6ad7fa5c970796e4cb42e08
mitto nagisa <mitto@mittostar.info>
2012-11-23 15:01:17 +0900

    base.shの中をお掃除してzlib-develとかもインストールするようにした

--- definitions/dev-centos/base.sh
+++ definitions/dev-centos/base.sh
@@ -2,25 +2,13 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
-#cat > /etc/yum.repos.d/epel.repo << EOM
-#[epel]
-#name=epel
-#baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
-#enabled=1
-#gpgcheck=0
-#EOM
-
-#yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget
-yum -y install kernel-devel
+yum -y install zlib-devel openssl-devel readline-devel sqlite-devel bzip2-devel kernel-devel
 
 yum -y update
 yum -y groupinstall "Development Tools"
-yum -y install rpmdev-tools
-
-yum -y install wget man vim ntp bind-utils telnet mlocate
 
 rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 rpm -ivh http://ftp.riken.jp/Linux/fedora/epel/6/i386/epel-release-6-7.noarch.rpm
 rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 
-yum -y install htop multitail
+yum -y install htop multitail perl wget man vim ntp bind-utils telnet mlocate rpmdev-tools
